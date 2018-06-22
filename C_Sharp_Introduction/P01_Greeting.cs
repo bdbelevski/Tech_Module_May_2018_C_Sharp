f004ee294b9388e0f9946a9c4599a7a482f91f11
bdbelevski <34931754+bdbelevski@users.noreply.github.com>
2018-06-22 21:05:10 +0300

    Add files via upload

---
+++ C_Sharp_Introduction/P01_Greeting.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class P01_Greeting
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
---
+++ C_Sharp_Introduction/P02_Add_Two_Numbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class P02_Add_Two_Numbers
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int sum = a + b;
+        Console.WriteLine($"{a} + {b} = {sum}");
+    }
+}
