59a8b259f91e11454e24cecb9b7f7fb43ce8ed2b
bdbelevski <34931754+bdbelevski@users.noreply.github.com>
2018-06-22 21:40:46 +0300

    Add files via upload

---
+++ CSharp_Cond_Statements_and_Loops/_02_Choose_a_Drink_2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class _02_Choose_a_Drink_2
+{
+    static void Main(string[] args)
+    {
+        string profession = Console.ReadLine();
+        int quantity = int.Parse(Console.ReadLine());
+        double price;
+
+        switch (profession)
+        {
+            case "Athlete":
+                price = 0.7;
+                break;
+            case "Businessman":
+            case "Businesswoman":
+                price = 1;
+                break;
+            case "SoftUni Student":
+                price = 1.7;
+                break;
+            default:
+                price = 1.2;
+                break;
+        }
+        double orderValue = quantity * price;
+        Console.WriteLine($"The {profession} has to pay {orderValue:F2}.");
+    }
+}
---
+++ CSharp_Cond_Statements_and_Loops/_03_Restaurant_Discount.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class _03_Restaurant_Discount
+{
+    static void Main(string[] args)
+    {
+        int groupSize = int.Parse(Console.ReadLine());
+        string package = Console.ReadLine();
+
+        double price = 0;
+        double pricePerPerson = 0;
+        string typeHall = "";
+
+        if (groupSize > 120)
+        {
+            Console.WriteLine("We do not have an appropriate hall.");
+            return;
+        }
+        else if (groupSize <= 50)
+        {
+            typeHall = "Small Hall";
+
+            if (package == "Normal")
+            { 
+                price = (2500 + 500) - (2500 + 500) * 0.05;
+                pricePerPerson = price / groupSize;
+            }
+            else if (package == "Gold")
+            {
+                price = (2500 + 750) - (2500 + 750) * 0.10;
+                pricePerPerson = price / groupSize;
+            }
+            else if (package == "Platinum")
+            {
+                price = (2500 + 1000) - (2500 + 1000) * 0.15;
+                pricePerPerson = price / groupSize;
+            }
+        }
+        else if(groupSize > 50 && groupSize <= 100)
+        {
+            typeHall = "Terrace";
+
+            if (package == "Normal")
+            {
+                price = (5000 + 500) - (5000 + 500) * 0.05;
+                pricePerPerson = price / groupSize;
+            }
+            else if (package == "Gold")
+            {
+                price = (5000 + 750) - (5000 + 750) * 0.10;
+                pricePerPerson = price / groupSize;
+            }
+            else if (package == "Platinum")
+            {
+                price = (5000 + 1000) - (5000 + 1000) * 0.15;
+                pricePerPerson = price / groupSize;
+            }
+        }
+        else if (groupSize > 100 && groupSize <= 120)
+        {
+            typeHall = "Great Hall";
+
+            if (package == "Normal")
+            {
+                price = (7500 + 500) - (7500 + 500) * 0.05;
+                pricePerPerson = price / groupSize;
+            }
+            else if (package == "Gold")
+            {
+                price = (7500 + 750) - (7500 + 750) * 0.10;
+                pricePerPerson = price / groupSize;
+            }
+            else if (package == "Platinum")
+            {
+                price = (7500 + 1000) - (7500 + 1000) * 0.15;
+                pricePerPerson = price / groupSize;
+            }
+        }
+        Console.WriteLine($"We can offer you the {typeHall}");
+        Console.WriteLine($"The price per person is {pricePerPerson:F2}$");
+    }
+}
+    
